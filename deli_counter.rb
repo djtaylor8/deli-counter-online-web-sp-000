2e330c8bd925b2e10544e49ff5317b049b8e876e
DJ Taylor <dtaylor88@gmail.com>
2021-05-31 04:22:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(array)
   if array == []
     puts "The line is currently empty."
   else
-    new_line = "The line is currently:" 
+    new_line = "The line is currently:"
     array.each_with_index do |name, index|
       new_line << " #{index + 1}. #{name}"
    end

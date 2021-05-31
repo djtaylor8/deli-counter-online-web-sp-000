8feabb7d2072306af570b91821f3f0cbb683e1ff
DJ Taylor <dtaylor88@gmail.com>
2021-05-31 03:43:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(array)
   if array == []
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{array.join(',')}"
+    puts "The line is currently: #{array.join('1.')}"
   end
 end
 
